c293b6876533928ca93e850840d74c16fb10ab04
Shrivatsan T <55741209+TheProgrammingArchive@users.noreply.github.com>
2025-07-17 07:34:29 +0530

    Update main.c

--- btree/main.c
+++ btree/main.c
@@ -6,17 +6,17 @@ int main(){
     tree.root = NULL;
     tree.order = 3;
 
-    insert(&tree, 5, "hello");
-    insert(&tree, 12, "hi");
-    insert(&tree, 1, "yo mama");
+    insert(&tree, 5, "1");
+    insert(&tree, 12, "2");
+    insert(&tree, 1, "3");
     insert(&tree, 2, "d");
     insert(&tree, 18, "erer");
     insert(&tree, 21, "test");
-     insert(&tree, 22, "fortnite");
+     insert(&tree, 22, "4");
      insert(&tree, 0, "trial");
      insert(&tree, 10, "hello");
     insert(&tree, 24, "hi");
-    // // insert(&tree, 11, "yo mama");
+    // // insert(&tree, 11, "6");
     // // insert(&tree, 19, "erer");
     // // insert(&tree, 28, "test");
     // // insert(&tree, 24, "fortnite");
