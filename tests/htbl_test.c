81878ea3a1e99a792b45017379e9f1653580080c
TheProgrammingArchive <shrivatsant@gmail.com>
2025-12-28 19:01:08 +0530

    feat: Begin writing tests for this modl

--- tests/htbl_test.c
+++ tests/htbl_test.c
@@ -2,12 +2,17 @@
 // Created by usrccr on 27/12/25.
 //
 #include <stddef.h>
+#include <unistd.h>
 
 #include "pcache.h"
 
 int main() {
     page_cache* cache = cache_init();
-    cache_page(cache, 0, NULL);
-    cache_page(cache, 0, NULL);
+    for (int i = 0; i < 20; i++)
+        cache_page(cache, i, &i);
+
+    page_fetch_result* res = fetch_page(cache, 16);
+    revoke_fetch(res);
+
     close_cache(cache);
 }
